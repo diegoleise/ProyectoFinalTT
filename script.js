dd24ec7fee21b0f552823b0ccd735722738f8e92
diegoleise <diegoleise@gmail.com>
2024-12-18 00:39:21 -0300

    modifico dark-mode

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-let botonOscuro = document.getElementById("dark-mode")
-let bodyoscuro = document.getElementById("body-tienda")
-console.log(botonOscuro)
-console.log(bodyoscuro)
 
-botonOscuro.addEventListener("click", function () {
-    if (botonOscuro.textContent == "Modo oscuro") {
-        bodyoscuro.style.backgroundColor = "black"
-        botonOscuro.style.color = "black"
-        botonOscuro.style.backgroundColor = "white"
-        botonOscuro.textContent = "Modo claro"
-    }
-    else if (bodyoscuro.style.backgroundColor == "black") {
-        bodyoscuro.style.backgroundColor = "bisque"
-        botonOscuro.textContent = "Modo oscuro"
-        botonOscuro.style.color = "white"
-        botonOscuro.style.backgroundColor = "black"
-    }
+const btnSwich = document.querySelector('#swich')
+console.log(btnSwich)
+btnSwich.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark');
+    btnSwich.classList.toggle('active')
 })
-//localStorage.setItem("darkMode", "botonOscuro")
+
+
+
+
+
+
+
 
 let productosTienda = [
     {

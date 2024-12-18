f6633c146fca854431e286fbddcab4f6d7471c70
diegoleise <diegoleise@gmail.com>
2024-12-18 14:12:41 -0300

    agrego localstorage para dark-mode

--- script.js
+++ script.js
@@ -3,8 +3,25 @@ const btnSwich = document.querySelector('#swich')
 console.log(btnSwich)
 btnSwich.addEventListener('click', ()=>{
     document.body.classList.toggle('dark');
-    btnSwich.classList.toggle('active')
+    btnSwich.classList.toggle('active');
+//guardamos el estado actual del boton
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem("dark-mode", "true");
+}else{
+    localStorage.setItem("dark-mode", "false");
+}
 })
+//recuperamos el estado del boton
+if (localStorage.getItem("dark-mode")=== "true"){
+    document.body.classList.add('dark');
+    btnSwich.classList.add('active');
+}
+    else{
+        document.body.classList.remove('dark');
+    }
+
+
+
 
 
 
